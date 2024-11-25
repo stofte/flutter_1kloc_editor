a75b57f1301f6adecfacb144f5c92a96c0c87fbb
Svend Tofte <stofte@gmail.com>
2024-11-25 18:44:57 +0100

    Initial IME hack

--- lib/document.dart
+++ lib/document.dart
@@ -2,9 +2,26 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
+class DocumentLocation {
+  int line;
+  int column;
+  DocumentLocation(this.line, this.column);
+
+  @override
+  bool operator ==(covariant DocumentLocation other) {
+    return other.line == line && other.column == column;
+  }
+
+  @override
+  int get hashCode => line.hashCode ^ column.hashCode;
+}
+
 class Document {
   List<String> lines = [''];
   List<double> widths = [];
+
+  DocumentLocation cursor = DocumentLocation(0, 0);
+  DocumentLocation anchor = DocumentLocation(0, 0);
 
   TextDirection textDirection = TextDirection.ltr;
   TextStyle style;
@@ -35,9 +52,73 @@ class Document {
     return true;
   }
 
+  // Inserts the text at the cursor position
+  (Offset, DocumentLocation) insertText(String text) {
+    var l = lines[cursor.line];
+    var l1st = l.characters.take(cursor.column);
+    var l2nd = l.characters.skip(cursor.column).take(l.characters.length - cursor.column);
+    lines[cursor.line] = "$l1st$text$l2nd";
+    tp.text = TextSpan(text: lines[cursor.line], style: style);
+    tp.layout();
+    widths[cursor.line] = tp.width;
+    cursor.column += text.characters.length;
+    return (Offset(tp.width, 0), cursor);
+  }
+
   Size getSize() {
     var h = lines.length * renderedGlyphHeight;
     var w = widths.fold(0.0, (max, val) => val > max ? val : max);
     return Size(w, h);
+  }
+
+  Offset getCursorOffset() {
+    var l = lines[cursor.line];
+    var before = l.characters.take(cursor.column).toString();
+    tp.text = TextSpan(text: before, style: style);
+    tp.layout();
+    return Offset(tp.width, cursor.line * renderedGlyphHeight);
+  }
+
+  bool setCursorFromOffset(Offset offset, double vScrollOffset, double hScrollOffset) {
+    // Assumes that the offset has been adjusted for canvas margins, etc
+    var lineNum = ((vScrollOffset + offset.dy) / renderedGlyphHeight).floor();
+    if (lineNum < 0) {
+      lineNum = 0;
+    } else if (lineNum >= lines.length) {
+      lineNum = lines.length - 1;
+    }
+    var colNum = _findCharIndexInStringFromOffset(lines[lineNum], offset.dx, 0);
+    var newCursor = DocumentLocation(lineNum, colNum);
+    var changed = cursor != newCursor;
+    cursor = newCursor;
+    return changed;
+  }
+
+  int _findCharIndexInStringFromOffset(String text, double offset, int index) {
+    if (text.isEmpty) {
+      return index;
+    } else if (text.characters.length == 1) {
+      // Find out which side of the char, we should return
+      tp.text = TextSpan(text: text, style: style);
+      tp.layout();
+      if (offset <= (tp.width / 2)) {
+        return index;
+      } else {
+        return index + 1;
+      }
+    } else {
+      // Text is at least two chars here
+      var midpoint = (text.characters.length / 2).floor();
+      var halfStr = text.characters.take(midpoint).toString();
+      tp.text = TextSpan(text: halfStr, style: style);
+      tp.layout();
+      var w = tp.width;
+      if (offset <= w) {
+        return _findCharIndexInStringFromOffset(halfStr, offset, index);
+      } else {
+        var otherHalfStr = text.characters.skip(midpoint).toString();
+        return _findCharIndexInStringFromOffset(otherHalfStr, offset - w, index + midpoint);
+      }
+    }
   }
 }
--- lib/editor.dart
+++ lib/editor.dart
@@ -18,9 +18,15 @@ class _EditorState extends State<Editor> {
 
   final ScrollController vScroll = ScrollController();
   final ScrollController hScroll = ScrollController();
+  final TextEditingController textController = TextEditingController();
   late DocumentProvider doc;
   late EditorNotifier notifier;
   late EditorConfig config;
+  final FocusNode imeFocusNode = FocusNode();
+  bool isMouseDown = false;
+
+  TextPainter imePainter = TextPainter(textDirection: TextDirection.ltr);
+  double imeWidth = 10;
 
   @override
   void initState() {
@@ -37,52 +43,129 @@ class _EditorState extends State<Editor> {
     // Triggers new build call when eg size of document changes
     doc.addListener(() => setState(() {}));
     doc.openFile(widget.path);
+    textController.addListener(() {
+      var newText = textController.text;
+      if (!textController.value.isComposingRangeValid && newText.isNotEmpty) {
+        textController.text = "";
+        doc.doc.insertText(newText);
+        setState(() {
+          imeWidth = 10;
+        });
+      } else {
+        imePainter.text = TextSpan(text: newText, style: config.textStyle);
+        imePainter.layout();
+        setState(() {
+          imeWidth = imePainter.width + 10; // TODO: Fudged cursor width?
+        });
+      }
+    });
+  }
+
+  void onPointerDown(PointerDownEvent event) {
+    var p = event.localPosition;
+    var offset = Offset(p.dx - config.canvasMargin, p.dy - config.canvasMargin);
+    if (doc.doc.setCursorFromOffset(offset, vScroll.offset, hScroll.offset)) {
+      doc.touch();
+    }
+    isMouseDown = true;
+  }
+
+  void onPointerMove(PointerMoveEvent event) {
+    if (isMouseDown) {
+      var p = event.localPosition;
+      var offset = Offset(p.dx - config.canvasMargin, p.dy - config.canvasMargin);
+      if (doc.doc.setCursorFromOffset(offset, vScroll.offset, hScroll.offset)) {
+        doc.touch();
+      }
+    }
+  }
+
+  void onPointerUp(PointerUpEvent event) {
+    var p = event.localPosition;
+    isMouseDown = false;
   }
 
   @override
   Widget build(BuildContext context) {
     stopwatch.reset();
+
+    // TODO: Fix this to be less hacky.
+    // Only true after initial build?
+    if (imeFocusNode.hasListeners) {
+      imeFocusNode.requestFocus();
+    }
+
     var winSize = MediaQuery.of(context).size;
+    var cursorOffset = doc.doc.getCursorOffset();
     var codeSize = doc.doc.getSize();
     codeSize = Size(codeSize.width + (config.canvasMargin * 2), codeSize.height + (config.canvasMargin * 2));
-    var ui = Stack(
-      children: [
-        Container(
-          color: Colors.white,
-          constraints: BoxConstraints(
-            maxHeight: winSize.height,
-            maxWidth: winSize.width,
+    var ui = Listener(
+      onPointerDown: onPointerDown,
+      onPointerMove: onPointerMove,
+      onPointerUp: onPointerUp,
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.white,
+            constraints: BoxConstraints(
+              maxHeight: winSize.height,
+              maxWidth: winSize.width,
+            ),
+            child: CustomPaint(
+              painter: EditorPainter(config, notifier),
+              child: Container(),
+            ),
           ),
-          child: CustomPaint(
-            painter: EditorPainter(config, notifier),
-            child: Container(),
-          ),
-        ),
-        SizedBox(
-          height: winSize.height,
-          width: winSize.width,
-          child: Scrollbar(
-            controller: vScroll,
+          SizedBox(
+            height: winSize.height,
+            width: winSize.width,
             child: Scrollbar(
-              controller: hScroll,
-              notificationPredicate: (notif) => notif.depth == 1,
-              child: SingleChildScrollView(
-                controller: vScroll,
-                scrollDirection: Axis.vertical,
+              controller: vScroll,
+              child: Scrollbar(
+                controller: hScroll,
+                notificationPredicate: (notif) => notif.depth == 1,
                 child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  controller: hScroll,
-                  child: SizedBox(
-                    height: codeSize.height,
-                    width: codeSize.width,
-                    child: Container(),
+                  controller: vScroll,
+                  scrollDirection: Axis.vertical,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    controller: hScroll,
+                    child: SizedBox(
+                      height: codeSize.height,
+                      width: codeSize.width,
+                      child: Container(),
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+          Positioned(
+            left: config.canvasMargin + cursorOffset.dx,
+            // TODO: 2 Is the diff between regular ascii lines and max height lines, but why?!
+            top: config.canvasMargin + cursorOffset.dy + 2,
+            // TODO: Width needs to be computed from the contents of the field ...
+            width: 100,
+            height: doc.doc.renderedGlyphHeight,
+            child: Container(
+              color: Color(0x66FFFF00),
+              child: EditableText(
+                enableIMEPersonalizedLearning: false,
+                enableInteractiveSelection: false,
+                controller: textController,
+                focusNode: imeFocusNode,
+                autofocus: true,
+                autocorrect: false,
+                style: config.textStyle,
+                scribbleEnabled: false,
+                cursorColor: Colors.blue,
+                backgroundCursorColor: Colors.transparent,
+                maxLines: 1,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
     print("build: ${stopwatch.elapsedMicroseconds} us");
     return ui;
